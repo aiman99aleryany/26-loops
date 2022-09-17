94b745e0a6d87f0910e3b6e3a94f36cbe1c0813a
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-17 19:24:18 +0300

    init commit

---
+++ exercise.js
@@ -0,0 +1,7 @@
+// Rewrite the exercise n. 24, by using the While loop.
+// exercise n. 24 was already written by a while loop, the task didn't specify to write a "for" loop
+// I use "while" loops because they are faster and require less code and they have more readability than the "for loop"
+let i = 0;
+while (++i <= 10) {
+  console.log(i);
+}
